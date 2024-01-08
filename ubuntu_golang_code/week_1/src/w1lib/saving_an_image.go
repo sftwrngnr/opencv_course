d16c8008754176009696240957412675b657b1bb
Dan Henderson <sftwrngnr66@gmail.com>
2024-01-08 06:43:32 -0700

    Added placeholder for SavingAnImage.

---
+++ ubuntu_golang_code/week_1/src/w1lib/saving_an_image.go
@@ -0,0 +1,35 @@
+package w1lib
+
+import (
+	"fmt"
+	"log"
+
+	"gocv.io/x/gocv"
+)
+
+func SavingAnImage() {
+	imagePath := DATA_PATH + "/images/number_zero.jpg"
+
+	// Read image in Grayscale format
+	testImage := gocv.IMRead(imagePath, 0)
+	PrintImageChannelValues(&testImage) //In Utils
+	myRoi, err := GetRoi(&testImage, 0, 3, 5, 6, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ROI is:\n")
+	PrintRoiValues(myRoi)
+	SetRoi(&testImage, 0, 3, 5, 6, 8, 111)
+
+	PrintImageChannelValues(&testImage)
+	window := gocv.NewWindow("Image as a Matrix")
+	defer window.Close()
+	window.IMShow(testImage)
+
+	for {
+		c := gocv.WaitKey(20)
+		if c == 27 {
+			break
+		}
+	}
+}
