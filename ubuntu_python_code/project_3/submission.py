3d80392f7f17925a3c713d93dd3d47ee4138d422
Dan Henderson <sftwrngnr66@gmail.com>
2024-01-15 14:17:16 -0700

    Updated submission. Still not quite working as expected.

--- ubuntu_python_code/project_3/submission.py
+++ ubuntu_python_code/project_3/submission.py
@@ -6,35 +6,53 @@ scaleValue = 1
 scaleType = 0
 maxType = 1
 scaleFactor = 1.0
+myShape = (200, 300, 3)
+stp = np.zeros(myShape, dtype=np.uint8)
+stp[:] = (128, 128, 128)
+origstp = stp.copy()
 
 windowName = "Resize Image"
 trackbarValue = "Scale"
 trackbarType = "Type: \n 0: Scale Up \n 1: Scale Down"
 
+
+def show_stype(sftype):
+    global stp
+    global origstp
+    stp = origstp.copy()
+    if sftype == False:
+        text = "Scale UP"
+    else:
+        text = "Scale Down"
+    fontScale = 1.5
+    fontFace = cv2.FONT_HERSHEY_COMPLEX
+    fontColor = (255, 255, 255)
+    fontThickness = 2
+    cv2.putText(stp, text, (5, 100), fontFace, fontScale, fontColor, fontThickness, cv2.LINE_AA);
+
+
+
 # load an image
 im = cv2.imread("./data/images/truth.png")
-myShape = (200, 200, 3)
-stp = np.zeros(myShape, dtype=np.uint8)
 scale_window = "ScalingFactor"
 #stp = cv2.cvtColor(stp, cv2.COLOR_BGR2GRAY)
-stp[:] = (128, 128, 128)
 cv2.namedWindow(
         scale_window,
         flags=( cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_FREERATIO | cv2.WINDOW_AUTOSIZE))
-cv2.resizeWindow(scale_window, 300, 200)
 cv2.moveWindow(scale_window, 640,100)
 # Create a window to display results
 cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
+show_stype(False)
 cv2.imshow(scale_window, stp)
 
-def ShowSType(sfType):
-    pass
 # Callback functions
 def scaleTypeImage(*args):
     global scaleType
     global scaleValue
     global scaleFactor
+    global scale_window
     scaleType = args[0]
+
     if scaleType == 1:
         scaleFactor = 1 - scaleValue/100.0
     else:
@@ -43,7 +61,8 @@ def scaleTypeImage(*args):
         scaleFactor = 1
     scaledImage = cv2.resize(im, None, fx=scaleFactor,\
             fy = scaleFactor, interpolation = cv2.INTER_LINEAR)
-    ShowSType(scaleFactor == 0)
+    show_stype(scaleFactor == 0)
+    cv2.imshow(scale_window, stp)
     cv2.imshow(windowName, scaledImage)
 
 def scaleImage(*args):
